220524e6e2154c3599b53b7b4d2673ef5198af75
GeraG18 <63253771+GeraG18@users.noreply.github.com>
2023-08-05 18:20:52 -0600

    [BASE] Beta interface preview working

---
+++ src/constants/constants.ts
@@ -0,0 +1,6 @@
+/* PROJECT AlpHa */
+/* w0rkiNg_ 4 tH3 pr0greSs */
+export const myName: string = 'Gerardo Gallegos, Jr.';
+
+export const landingTitle: string = 'Hey there!';
+export const landingDescription: string = 'Web developer 💻 who ';
